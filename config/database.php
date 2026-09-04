86076ace4f7a400220a84dca6ebacf998c3a9fe4
gilberas <gillyimo2009@gmail.com>
2026-09-04 13:58:06 +0300

    feat: add table prefix to avoid collisions on shared db

--- config/database.php
+++ config/database.php
@@ -109,7 +109,7 @@ return [
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => env('DB_CHARSET', 'utf8'),
-            'prefix' => '',
+            'prefix' => 'inv_',
             'prefix_indexes' => true,
             'search_path' => 'public',
         ],
